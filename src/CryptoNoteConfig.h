ae830c427d7d30fcc2eebcae5c05615bc2770e96
nightfury626 <34405049+nightfury626@users.noreply.github.com>
2018-02-28 00:57:43 -0600

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "Pharaoh";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001c89edab23c029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121011fa96f689a5ef4cc0b8166d9f188908a9ac027e28c7eeb9a93aff694670ea7f4";
 
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
